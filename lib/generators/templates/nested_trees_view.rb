2a5b79d76f602b2072c294bcb8eab72d2d31a682
Alex Anzelm <anex.work@gmail.com>
2013-08-06 15:43:15 +0400

    Change template (pluralize -> tableize)

--- lib/generators/templates/nested_trees_view.rb
+++ lib/generators/templates/nested_trees_view.rb
@@ -1,35 +1,35 @@
 class NestedTreesViewTo<%= model_name.pluralize %> < ActiveRecord::Migration
   def up
     execute %(
-    CREATE OR REPLACE VIEW view_<%= model_name.pluralize %> AS
-      SELECT count(<%= model_name.pluralize %>.left_key) - 1 AS level,
+    CREATE OR REPLACE VIEW view_<%= model_name.tableize %> AS
+      SELECT count(<%= model_name.tableize %>.left_key) - 1 AS level,
         <%= (options.key_fields + %w(left_key right_key id)).map do |key|
-              %(#{model_name.pluralize}.#{key})
-            end.join ','%>
-        ( SELECT <%= model_name.pluralize %>_parent.post_id
-           FROM <%= model_name.pluralize %> <%= model_name.pluralize %>_parent
-          WHERE <%= model_name.pluralize %>_parent.left_key < <%= model_name.pluralize %>.left_key
-            AND <%= model_name.pluralize %>_parent.right_key > <%= model_name.pluralize %>.right_key
+              %(#{model_name.tableize}.#{key})
+            end.join ', '%>
+        ( SELECT <%= model_name.tableize %>_parent.post_id
+           FROM <%= model_name.tableize %> <%= model_name.tableize %>_parent
+          WHERE <%= model_name.tableize %>_parent.left_key < <%= model_name.tableize %>.left_key
+            AND <%= model_name.tableize %>_parent.right_key > <%= model_name.tableize %>.right_key
             <%= options.key_fields.map do |key|
-              %( AND #{model_name.pluralize}.#{key} = #{model_name.pluralize}_parent.#{key})
+              %( AND #{model_name.tableize}.#{key} = #{model_name.tableize}_parent.#{key})
             end.join%>
-          ORDER BY <%= model_name.pluralize %>.left_key DESC
+          ORDER BY <%= model_name.tableize %>.left_key DESC
          LIMIT 1) AS parent
-       FROM <%= model_name.pluralize %>, <%= model_name.pluralize %> <%= model_name.pluralize %>_parent
-       WHERE <%= model_name.pluralize %>.left_key >= <%= model_name.pluralize %>_parent.left_key
-         AND <%= model_name.pluralize %>.left_key <= <%= model_name.pluralize %>_parent.right_key
+       FROM <%= model_name.tableize %>, <%= model_name.tableize %> <%= model_name.tableize %>_parent
+       WHERE <%= model_name.tableize %>.left_key >= <%= model_name.tableize %>_parent.left_key
+         AND <%= model_name.tableize %>.left_key <= <%= model_name.tableize %>_parent.right_key
          <%= options.key_fields.map do |key|
-            %( AND #{model_name.pluralize}.#{key} = #{model_name.pluralize}_parent.#{key})
+            %( AND #{model_name.tableize}.#{key} = #{model_name.tableize}_parent.#{key})
           end.join%>
        GROUP BY
          <%= (options.key_fields + %w(left_key right_key id)).map do |key|
-              %(#{model_name.pluralize}.#{key})
-            end.join ','%>
-       ORDER BY <%= model_name.pluralize %>.left_key;
+              %(#{model_name.tableize}.#{key})
+            end.join ', '%>
+       ORDER BY <%= model_name.tableize %>.left_key;
     )
   end
   
   def down
-    execute %(DROP VIEW view_<%= model_name.pluralize %>;)
+    execute %(DROP VIEW view_<%= model_name.tableize %>;)
   end
 end
