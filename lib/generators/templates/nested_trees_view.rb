88a385bf8c116c02ec8d6f4c618b2409182d11b7
Alex Anzelm <anex.work@gmail.com>
2013-08-06 15:51:25 +0400

    Adding value_field options

--- lib/generators/templates/nested_trees_view.rb
+++ lib/generators/templates/nested_trees_view.rb
@@ -3,10 +3,10 @@ class NestedTreesViewTo<%= model_name.pluralize %> < ActiveRecord::Migration
     execute %(
     CREATE OR REPLACE VIEW view_<%= model_name.tableize %> AS
       SELECT count(<%= model_name.tableize %>.left_key) - 1 AS level,
-        <%= (options.key_fields + %w(left_key right_key id)).map do |key|
+        <%= (options.key_fields + [options.value_field] + %w(left_key right_key id)).map do |key|
               %(#{model_name.tableize}.#{key})
-            end.join ', '%>
-        ( SELECT <%= model_name.tableize %>_parent.post_id
+          end.join ', '%>,
+        ( SELECT <%= model_name.tableize %>_parent.<%= options.value_field =>
            FROM <%= model_name.tableize %> <%= model_name.tableize %>_parent
           WHERE <%= model_name.tableize %>_parent.left_key < <%= model_name.tableize %>.left_key
             AND <%= model_name.tableize %>_parent.right_key > <%= model_name.tableize %>.right_key
@@ -22,7 +22,7 @@ class NestedTreesViewTo<%= model_name.pluralize %> < ActiveRecord::Migration
             %( AND #{model_name.tableize}.#{key} = #{model_name.tableize}_parent.#{key})
           end.join%>
        GROUP BY
-         <%= (options.key_fields + %w(left_key right_key id)).map do |key|
+         <%= (options.key_fields + [options.value_field] + %w(left_key right_key id)).map do |key|
               %(#{model_name.tableize}.#{key})
             end.join ', '%>
        ORDER BY <%= model_name.tableize %>.left_key;
