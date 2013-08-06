0871c13ee313a228859ee13622584a7ea5c6a01c
Alex Anzelm <anex.work@gmail.com>
2013-08-06 15:16:30 +0400

    Pre-alpha generator

---
+++ lib/generators/nested_trees_generator.rb
@@ -0,0 +1,35 @@
+class NestedTreesViewTo<%= model_name.pluralize %> < ActiveRecord::Migration
+  def up
+    execute %(
+    CREATE OR REPLACE VIEW view_<%= model_name.pluralize %> AS
+      SELECT count(<%= model_name.pluralize %>.left_key) - 1 AS level,
+        <%= (key_fields + %w(left_key right_key id)).map do |key|
+              %(#{model_name.pluralize}.#{key})
+            end.join ','%>
+        ( SELECT <%= model_name.pluralize %>_parent.post_id
+           FROM <%= model_name.pluralize %> <%= model_name.pluralize %>_parent
+          WHERE <%= model_name.pluralize %>_parent.left_key < <%= model_name.pluralize %>.left_key
+            AND <%= model_name.pluralize %>_parent.right_key > <%= model_name.pluralize %>.right_key
+            <%= key_fields.map do |key|
+              %( AND #{model_name.pluralize}.#{key} = #{model_name.pluralize}_parent.#{key})
+            end.join%>
+          ORDER BY <%= model_name.pluralize %>.left_key DESC
+         LIMIT 1) AS parent
+       FROM <%= model_name.pluralize %>, <%= model_name.pluralize %> <%= model_name.pluralize %>_parent
+       WHERE <%= model_name.pluralize %>.left_key >= <%= model_name.pluralize %>_parent.left_key
+         AND <%= model_name.pluralize %>.left_key <= <%= model_name.pluralize %>_parent.right_key
+         <%= key_fields.map do |key|
+            %( AND #{model_name.pluralize}.#{key} = #{model_name.pluralize}_parent.#{key})
+          end.join%>
+       GROUP BY
+         <%= (key_fields + %w(left_key right_key id)).map do |key|
+              %(#{model_name.pluralize}.#{key})
+            end.join ','%>
+       ORDER BY <%= model_name.pluralize %>.left_key;
+    )
+  end
+  
+  def down
+    execute %(DROP VIEW view_<%= model_name.pluralize %>;)
+  end
+end
---
+++ lib/generators/templates/nested_trees_view.rb
@@ -0,0 +1,16 @@
+class NestedTreesGenerator < Rails::Generators::Base
+  include Rails::Generators::Migration
+  
+  desc 'Generate migration view for model'
+  argument :model_name, :type => :string
+  class_option :key_fields, :type => :array, :default => [], :desc => 'additional nested key'
+  source_root File.expand_path('../templates', __FILE__)
+  
+  def self.next_migration_number(dirname)
+    Time.now.utc.strftime("%Y%m%d%H%M%S")
+  end
+  
+  def create_migration_file
+    migration_template 'nested_trees_generator.rb', %(db/migrate/nested_trees_view_to_#{model_name.tableize}.rb)
+  end
+end
